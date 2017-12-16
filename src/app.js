559ac4608377fd1a4e33c481e1c07b45d2419870
Jase Bleazard <jase.bleazard@outlook.com>
2017-12-16 09:31:49 -0700

    Fixed the warnings about not handling promises

--- src/app.js
+++ src/app.js
@@ -16,33 +16,35 @@ app.post('/', upload.single('thumb'), (req, res, next)=>{
     console.log("Player Title:\t" + payload.Player.title);
     console.log("Media Title:\t" + payload.Metadata.title);
     
-    fs.appendFile("request.log", JSON.stringify(payload), function(err) {
-	if (err) {
-	    return console.log(err);
-	}
-    });
+//    fs.appendFile("request.log", JSON.stringify(payload), function(err) {
+//	if (err) {
+//	    return console.log(err);
+//	}
+//    });
 
-    run(payload);
+    run(payload).catch((err)=>{console.log(err)});
 });
 app.listen(12035);
 
 async function run(payload) {
-    if (!config.ApprovedClients[payload.Player.uuid]) {
+    if (!config.AprovedClients[payload.Player.uuid]) {
+	console.log("LIGHTS NOT APPROVED");
 	return;
     }
+    
     const client = new Client();
     const device = await client.getDevice({host: config.Host});
 
     switch (payload.event) {
-        case 'media.play':
-        case 'media.resume': {
-	    device.setPowerState(false);
-	    break;
-        }
-        case 'media.stop':
-        case 'media.pause': {
-	    device.setPowerState(true);
-	    break;
-        }
+    case 'media.play':
+    case 'media.resume':
+	device.setPowerState(false);
+	console.log("Lights :: OFF");
+	break;
+    case 'media.stop':
+    case 'media.pause':
+	device.setPowerState(true);
+	console.log("Lights :: ON");
+	break;
     }
 }
